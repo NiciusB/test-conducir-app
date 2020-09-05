6eeee63737fd6d727af5f8d8ad550e7e755caeec
NiciusB <nuno@balbona.me>
2020-09-05 11:51:35 +0200

    Fix pm2

--- client/src/lib/loadRandomQuestion.js
+++ client/src/lib/loadRandomQuestion.js
@@ -1,3 +1,6 @@
 export default async function loadRandomQuestion () {
-  return await fetch(process.env.REACT_APP_API_URL + '/question').then(res => res.json())
+  return await fetch(process.env.REACT_APP_API_URL + '/question')
+    .catch(() => { throw new Error('Unable to reach API') })
+    .then(res => res.json())
+    .catch(() => { throw new Error('Unable to parse API response') })
 }
